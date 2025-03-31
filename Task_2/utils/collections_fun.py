cf7477e4c124837dd7684d2f14dcc30c179afaef
AlexDvor <alexmatvichuk@gmail.com>
2025-03-31 19:41:07 +0200

    add funs

--- Task_2/utils/collections_fun.py
+++ Task_2/utils/collections_fun.py
@@ -1,11 +1,23 @@
 from decorators.error_handlers import input_error
+from models.address_book import AddressBook
+from models.record import Record
 
 
 @input_error
-def add_contact(args, contacts: dict):
-    name, phone = args
-    contacts[name] = phone
-    return f"Contact {name} added!"
+def add_contact(args, book: AddressBook):
+
+    name, phone, *_ = args
+    record = book.find(name)
+    message = "Contact updated."
+
+    if record is None:
+        record = Record(name)
+        book.add_record(record)
+        message = "Contact added."
+
+    if phone:
+        record.add_phone(phone)
+    return message
 
 
 @input_error
@@ -36,3 +48,33 @@ def show_all(_, contacts):
         return "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
     else:
         return "Your list is empty!"
+
+
+def add_birthday(args, book: AddressBook):
+    name, birthday_date = args
+    record = book.find(name)
+    if record:
+        record.add_birthday(birthday_date)
+        return f"Birthday for {name} added!"
+    else:
+        return "Contact not found!"
+
+
+def show_birthday(args, book: AddressBook):
+    name = args[0]
+    record = book.find(name)
+    if record and record.birthday:
+        return f"{name}'s birthday is on {record.birthday.value.strftime('%d.%m.%Y')}"
+    return "Contact not found or birthday not set!"
+
+
+def birthdays(_, book: AddressBook):
+    upcoming = book.get_upcoming_birthdays()
+    if not upcoming:
+        return "No upcoming birthdays this week."
+    return "\n".join(
+        [
+            f"{rec.name.value}: {rec.birthday.value.strftime('%d.%m.%Y')}"
+            for rec in upcoming
+        ]
+    )
